125bb6009038d543ead1e514574b4d3a67e9257e
brogrammer232 <raymondmwaura232@gmail.com>
2025-08-25 11:26:02 +0300

    Wrote the code for the 'scheduler' module. Can schedule review dates.

---
+++ src/scheduler.py
@@ -0,0 +1,30 @@
+"""
+"""
+
+from datetime import datetime, timedelta
+
+REVIEW_DATE_DELTAS = [2, 6, 13, 29, 59, 119]
+DATE_FORMAT = "%Y-%m-%d"
+
+def schedule_review_dates(study_date: str) -> dict[str, list[str]]:
+    """
+    Generate spaced review dates starting from a given study date.
+
+    :param study_date: A string representing the study date in "YYYY-MM-DD" format.
+    :return: A mapping from the study date to a list of review dates.
+    """
+  
+    try:
+        study_date = datetime.strptime(study_date, DATE_FORMAT).date()
+    except ValueError:
+        raise ValueError(
+            f"Invalid date format: `{study_date}`.Expected format is 'YYYY-MM-DD'."
+        )
+
+    # Calculate review dates.
+    return {
+        study_date.strftime(DATE_FORMAT): [
+            (study_date + timedelta(days = review_date_delta)).strftime(DATE_FORMAT)
+            for review_date_delta in REVIEW_DATE_DELTAS
+        ]
+    }

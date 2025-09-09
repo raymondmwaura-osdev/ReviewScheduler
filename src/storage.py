0c288205289578d1bcb134b16212bdaf268eda20
brogrammer232 <raymondmwaura232@gmail.com>
2025-09-09 14:20:09 +0300

    Added the  function.

--- src/storage.py
+++ src/storage.py
@@ -9,9 +9,24 @@ def backup_file(file: str) -> None:
     """
     pass
 
+def append_json(file: str | Path, content: dict) -> None:
+    """
+    Append the given dictionary to the dictionary in the given JSON file.
+
+    :param file: JSON file to append to. Can be a string or a `pathlib.Path` object.
+    :param content: A dictionary containing the information to append to the file.
+    """
+    if not isinstance(file, (str, Path)):
+        raise ValueError("Expected `str` or `Path` for `file`.")
+
+    file = Path(file)
+    main_dict = read_json(file)
+    main_dict = main_dict | content
+    print(main_dict)
+
 def read_json(file: str | Path) -> dict[str, list[str]]:
     """
-    Read the given json file and return a Python dictionary.
+    Read the given JSON file and return a Python dictionary.
     Return an empty dictionary if the given file does not exist.
 
     :param file: JSON file to read. Can be a string or a `pathlib.Path` object.
@@ -24,5 +39,5 @@ def read_json(file: str | Path) -> dict[str, list[str]]:
         return json.loads(file.read_text(encoding="utf-8"))
 
     except FileNotFoundError:
-        print(f"WARNING: File not found: {file}.\nProceeding with empty content.")
+        print(f"WARNING: File not found: {file}. Proceeding with empty content.")
         return {}

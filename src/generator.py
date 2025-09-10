da20b96c09e2191b187bdb22a8077477d4180de6
brogrammer232 <raymondmwaura232@gmail.com>
2025-09-10 13:42:08 +0300

    Added the 'locate_vault' function.

--- src/generator.py
+++ src/generator.py
@@ -1,15 +1,16 @@
 from scheduler import schedule_review_dates
-from storage import append_json
 from datetime import datetime, date
+from storage import append_json
+from pathlib import Path
 import sys
 
 DATE_FORMAT = "%Y-%m-%d"
 
 def add_study_date(study_date: str) -> None:
     """
-    + Take a study date.
-    + Generate review dates.
-    + Update `daily.json`.
+    Generate review dates for the given study date and save them in `daily.json`.
+
+    :param study_date: Date of study in the format: "YYYY-MM-DD".
     """
 
     # Validate study date.
@@ -23,7 +24,29 @@ def add_study_date(study_date: str) -> None:
 
     # Generate and save review dates.
     review_dates = schedule_review_dates(study_date)
-    append_json("daily.json", {study_date.strftime(DATE_FORMAT):review_dates}) 
+
+    scheduler_directory = locate_vault()
+    json_file = scheduler_directory / "schedule/daily.json"
+    append_json(json_file, {study_date.strftime(DATE_FORMAT):review_dates}) 
+
+def locate_vault() -> Path:
+    """
+    Locate the `.scheduler` directory in the current directory tree.
+    Exit if `.scheduler` isn't found or is not a directory.
+
+    :return: A `Path` object of the `.scheduler` directory.
+    """
+    current_directory = Path.cwd()
+
+    for parent in [current_directory, *current_directory.parents]:
+        vault_path = parent / ".scheduler"
+        if vault_path.exists() and vault_path.is_dir():
+            return vault_path
+
+    sys.exit(
+        "Scheduler is not initialized in the current directory tree.\n"
+        "Use `scheduler init` to initialize it in the current working directory."
+    )
 
 def parse_date(date_str: str) -> date:
     """
@@ -37,6 +60,7 @@ def parse_date(date_str: str) -> date:
         return datetime.strptime(date_str, DATE_FORMAT).date()
 
     except ValueError:
-        print(f"Invalid date format: {study_date}\nExpected format: YYYY-MM-DD",
-            file=sys.stderr)
-        sys.exit()
+        sys.exit(
+            f"Invalid date format: {study_date}\n"
+            "Expected format: YYYY-MM-DD"
+        )

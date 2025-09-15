2cd06a51593ac05da36ae87d24c757f1b86d4737
brogrammer232 <raymondmwaura232@gmail.com>
2025-09-15 11:59:23 +0300

    Wrote file path relative to the vault directory.

--- src/utilities/constants.py
+++ src/utilities/constants.py
@@ -2,5 +2,7 @@
 DATE_FORMAT = "%Y-%m-%d"
 
 VAULT = ".rs"
-STUDY_REVIEW_FILE = "study_review.json"
-REVIEW_STUDY_FILE = "review_study.json"
+
+# Written relative to VAULT.
+STUDY_REVIEW_FILE = "./study_review.json"
+REVIEW_STUDY_FILE = "./review_study.json"

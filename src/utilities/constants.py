97320c0fd300d688ba04136eb05c683e84627388
brogrammer232 <raymondmwaura232@gmail.com>
2025-09-17 09:58:12 +0300

    Wrote file paths relative to VAULT's parent directory.

--- src/utilities/constants.py
+++ src/utilities/constants.py
@@ -3,6 +3,6 @@ DATE_FORMAT = "%Y-%m-%d"
 
 VAULT = ".rs"
 
-# Written relative to VAULT.
-STUDY_REVIEW_FILE = "./study_review.json"
-REVIEW_STUDY_FILE = "./review_study.json"
+# Written relative to VAULT's parent.
+STUDY_REVIEW_FILE = ".rs/study_review.json"
+REVIEW_STUDY_FILE = ".rs/review_study.json"

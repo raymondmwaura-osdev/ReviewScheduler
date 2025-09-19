69ed4c41860781ee0318015ed1e68b103f2769c6
brogrammer232 <raymondmwaura232@gmail.com>
2025-09-19 11:27:23 +0300

    Added directory constants.

--- src/utilities/constants.py
+++ src/utilities/constants.py
@@ -1,8 +1,10 @@
 
 DATE_FORMAT = "%Y-%m-%d"
 
+## FILESYSTEM
 VAULT = ".rs"
+REVIEW_STUDY_FILE = f"{VAULT}/review_study.json"
+STUDY_REVIEW_FILE = f"{VAULT}/study_review.json"
 
-# Written relative to VAULT's parent.
-STUDY_REVIEW_FILE = ".rs/study_review.json"
-REVIEW_STUDY_FILE = ".rs/review_study.json"
+BACKUP_DIRECTORY = f"{VAULT}/backup"
+HISTORY_DIRECTORY = f"{VAULT}/history"
